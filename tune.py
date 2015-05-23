54c2413a639fcda473c0dfbda40762ad35655229
imfede <federico.cergol@gmail.com>
2015-05-23 11:28:53 -0600

    Added functions, added zanar

--- tune.py
+++ tune.py
@@ -13,23 +13,33 @@ GPIO.setup( buttonpin, GPIO.IN, pull_up_down=GPIO.PUD_UP )
 
 def play( filename ):
     speed = 60 / getTempo( filename )
+    tuning = getTuning(filename)
+    inter = getInter( filename )
     p = GPIO.PWM( pin, 0.5)
-    p.start(1)
     for note in getTune( filename ):
         if( note[0] > 10 ):
-            p.ChangeFrequency( note[0] )
-        else:
-            p.stop()
+            p.start(1)
+            p.ChangeFrequency( note[0]*tuning )
         t = time.time()
         stopt = note[1]*speed - 0.01
         while (time.time() - t) < (stopt):
             if not GPIO.input( buttonpin ):
                 return False
             time.sleep( 0.05 )
-        if( note[0] <= 10 ):
-            p.start(1)
-    p.stop()
+        if( note[0] > 10 ):
+            p.stop()
+        time.sleep( inter )
     return True
+
+def getInter(filename):
+    with open(filename, 'r') as jfile:
+        data = json.load( jfile )
+        return data['inter']
+
+def getTuning(filename):
+    with open(filename, 'r') as jfile:
+        data = json.load( jfile )
+        return data['tuning']
 
 def getTune(filename):
     with open(filename, 'r') as jfile:
@@ -42,9 +52,9 @@ def getTempo(filename):
         return data['tempo']
 
 try:
-    while play( 'tetris.json' ):
+    while play( sys.argv[1] ):
         time.sleep(1)
 except KeyboardInterrupt:
-    pass
+    pass # do nothing
 finally:
     GPIO.cleanup()
